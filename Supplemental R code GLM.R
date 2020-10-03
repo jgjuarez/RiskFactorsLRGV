975afb1c66654249c9b0e431f8eeed253690f0dd
Jose G Juarez <jjuarezvaldez@gmail.com>
2020-10-02 20:22:23 -0500

    Night update of October 2nd

--- Supplemental R code GLM.R
+++ Supplemental R code GLM.R
@@ -46,6 +46,113 @@ Risk$Shade <- relevel(as.factor(Risk$Shade), ref = "Half shade")
 
 #For this procedure we will do a Generalized linear Model analysis
 #Evaluating the best distribution for the dataset with count data for the indoor collections
+RisInPs1 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + AP2.2 + Window1 + Window2 + Door1 + Door2 
+                    , family = "poisson", data = Risk)
+
+#Evaluation of the overdispersion of the dataset 
+check_overdispersion(RisInPs1)
+
+RisInNB1 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + AP2.2 + Window1 + Window2 + Door1 + Door2 
+                    , family = "nbinom1", data = Risk)
+
+RisInNB2 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + AP2.2 + Window1 + Window2 + Door1 + Door2 
+                    , family = "nbinom2", data = Risk)
+
+#Visualization of the data
+RisInPsV1 <- simulateResiduals(fittedModel = RisInPs1, n = 250, plot = TRUE)
+RisInNB1V1 <- simulateResiduals(fittedModel = RisInNB1, n = 250, plot = TRUE)
+RisInNB2V1 <- simulateResiduals(fittedModel = RisInNB2, n = 250, plot = TRUE)
+
+#Checking the AIC values for each model
+AIC(RisInPs1)
+AIC(RisInNB1)
+AIC(RisInNB2)
+
+#explore interaction contrasts of best fit with a NB2 distribution
+
+m1 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + AP2.2 + Window1 + Window2 + Door1 + Door2 
+              , family = "nbinom2", data = Risk)
+car::Anova(m1, type ="III")
+
+m2 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + AP2.2 + Window1 + Window2 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m2, type ="III")
+
+m3 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + Window1 + Window2 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m3, type ="III")
+
+m4 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenDoorFreq + WaterStorage + OtherContainers + Income+ AP2.1 + Window1 + Window2 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m4, type ="III")
+
+m5 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + WaterStorage + OtherContainers + Income+ AP2.1 + Window1 + Window2 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m5, type ="III")
+
+m6 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OtherContainers + Income+ AP2.1 + Window1 + Window2 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m6, type ="III")
+
+m7 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OtherContainers + Income+ AP2.1 + Window1 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m7, type ="III")
+
+m8 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + Income+ AP2.1 + Window1 + Door1
+              , family = "nbinom2", data = Risk)
+car::Anova(m8, type ="III")
+
+AICtab(m1,m2,m3,m4,m5,m6,m7,m8, base = T, weights = T, sort = FALSE)
+
+summary(m7)
+
+results <- check_collinearity(m7)
+results
+plot(results)
+
+#This next code takes a lot of time, skip to the joint_test and plot effects if already tested
+df1.1 <- tidy(m7)
+
+df1.2 <- tidy(confint(m7, method = "uniroot"))
+
+
+#Creating the figures
+
+my_title <- expression(paste("Indoor female ", italic("Ae. aegypti")))
+
+plot_model(m7, vline.color = "lightgray", transform = "plogis", sort.est = TRUE, show.value = TRUE, value.offset = .3, title = my_title)
+
+plot_model(m7, vline.color = "lightgray", sort.est = TRUE, show.value = TRUE, value.offset = .3, title = my_title)
+
+plot_model(m7, type = "pred", axis.title = my_title, title = "")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          ###THIS IS THE PREVIOUS CODE
+
+
+
+#For this procedure we will do a Generalized linear Model analysis
+#Evaluating the best distribution for the dataset with count data for the indoor collections
 RisInPs1 <- glmmTMB(AEinFem ~ offset(log(WeeksIN)) + TypeAC + OpenWindowsFreq + WaterStorage + OtherContainers + AP2.1 + AP2.2 + Window1 + Window2 + Door1 + Door2 
                     , family = "poisson", data = Risk)
 
